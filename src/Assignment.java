1f2e8d5d366519590ef780f5c4d06d1da184ab51
zubaida-Busur <zubaidayaxye2011@gmail.com>
2021-02-22 22:43:22 +0300

    submit assignment

--- src/Assignment.java
+++ src/Assignment.java
@@ -2,21 +2,12 @@ import java.util.Scanner;
 public class Assignment {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("enter a number");
-        int num1 = in.nextInt();
-        int num2 = in.nextInt();
-        int num3 = in.nextInt();
+        int i;
+        System.out.println("natural numbers from 1 to 10");
+        for (i=1;i<=10;i++)
+        System.out.println(i);
 
-        if(num1>num2)
-             if(num1>num3)
-                System.out.println("the greatest is" +num1);
-             if(num2>num1)
-             if(num2>num3)
-                System.out.println("the greatest no is" +num2);
-            if(num3>num1)
-            if(num3>num2)
-                System.out.println("the greatest no is" +num3);
+
 
 
 
