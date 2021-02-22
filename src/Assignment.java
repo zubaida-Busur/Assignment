63bea1c95c0f3cdca66d5275af37a15cfd9d28b0
zubaida-Busur <zubaidayaxye2011@gmail.com>
2021-02-22 21:33:35 +0300

    submit assignment

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 public class Assignment {
 
     public static void main(String[] args)
     {
-            //Salaam
+        Scanner in=new Scanner(System.in);
+         int i,numbers=0, sum=0;
+         double avg;
+        System.out.println("enter any 5 numbers");
+
+        for( i=0; i<5; i++){
+            numbers=in.nextInt();
+            sum=sum+numbers;
+        }
+        avg=sum/5;
+        System.out.println("the sum of 5 no is:"+sum+ "\n average is:"+avg);
+
     }
-}
+
+
+    }
+
