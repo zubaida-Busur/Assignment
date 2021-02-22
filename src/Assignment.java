eaa33528b06de426f8d5fe9421b42f1f465dbace
zubaida-Busur <zubaidayaxye2011@gmail.com>
2021-02-23 01:08:47 +0300

    submit assignment

--- src/Assignment.java
+++ src/Assignment.java
@@ -2,22 +2,27 @@ import java.util.Scanner;
 public class Assignment {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input a month number: ");
-        int month = input.nextInt();
-        if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12)
-            System.out.println("31 days in the month");
-        else if(month==4 || month==6 || month==9 || month==11 )
-            System.out.println("30 days in the month");
-        else if(month==2)
-            System.out.println("either 28 or 29 days in month");
-
-
-
-
-
-
+        double x = 12.5;
+        double y = 13.6;
+        double z = 18.9;
+        double findavg = average(12.5, 13.6, 18.9);
+        System.out.println(findavg);
 
     }
+
+
+        public static double average(double x, double y, double z)
+        {
+            double avg=(x + y + z) / 3;
+            return avg;
+        }
+
+
+
+
+
+
+
+
 }
 
