2584fc83826426f2610e5753b17d8f254c68ddfa
zubaida-Busur <zubaidayaxye2011@gmail.com>
2021-02-22 23:05:21 +0300

    submit assignment

--- src/Assignment.java
+++ src/Assignment.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 public class Assignment {
 
     public static void main(String[] args) {
-        int i;
-        System.out.println("natural numbers from 1 to 10");
-        for (i=1;i<=10;i++)
-        System.out.println(i);
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input a month number: ");
+        int month = input.nextInt();
+        if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12)
+            System.out.println("31 days in the month");
+        else if(month==4 || month==6 || month==9 || month==11 )
+            System.out.println("30 days in the month");
+        else if(month==2)
+            System.out.println("either 28 or 29 days in month");
 
 
 
